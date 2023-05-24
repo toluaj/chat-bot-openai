d35f46c17ab8a2b637c5a227de12acd6a085bbf7
Tolu Ajia <toluajia@Tolus-MacBook-Pro-2.local>
2023-05-24 12:16:27 +0100

    chore: add analytics to site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { Box, ChakraProvider } from '@chakra-ui/react'
 import Chat from './screens/SplashScreen';
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <ChakraProvider>
       <Box>
         <Chat />
+        <Analytics />
       </Box>
     </ChakraProvider>
   );
